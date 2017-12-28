9d7f6c042daf515d6f7c2b6ad19ee5167b86b203
Mohit Mulchandani <Mohit.Mulchandani@asu.edu>
2017-12-28 21:53:47 +0530

    Web customer tracker CRM project setup and connection testing

---
+++ web-customer-tracker/src/com/mohit/testdb/TestDbServlet.java
@@ -0,0 +1,43 @@
+package com.mohit.testdb;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import java.io.IOException;
+
+import java.io.PrintWriter;
+import java.sql.*;
+
+@WebServlet("/TestDbServlet")
+public class TestDbServlet extends javax.servlet.http.HttpServlet {
+    protected void doPost(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+
+    }
+
+    protected void doGet(javax.servlet.http.HttpServletRequest request, javax.servlet.http.HttpServletResponse response) throws javax.servlet.ServletException, IOException {
+        // setup connection variables
+        String user = "springstudent";
+        String pass = "springstudent";
+
+        String jdbcUrl = "jdbc:mysql://localhost:3306/web_customer_tracker?useSSL=false";
+        String driver = "com.mysql.jdbc.Driver";
+
+        // get connection to database
+
+        try{
+            PrintWriter out = response.getWriter();
+
+            out.println("Connecting to database: " + jdbcUrl);
+            Class.forName(driver);
+
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, pass);
+
+            out.println("SUCCESS!!");
+
+
+            myConn.close();
+        }catch (Exception e){
+            e.printStackTrace();
+            throw new ServletException(e);
+        }
+    }
+}
---
+++ web-customer-tracker/web/WEB-INF/view/list-customers.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: mohitmulchandani
+  Date: 12/28/17
+  Time: 9:45 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>List Customers</title>
+</head>
+<body>
+List Customers - coming soon...
+</body>
+</html>
